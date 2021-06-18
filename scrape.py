cadfdf6024937b62b5a77bb53e5f0667aaf98b5a
Aryan <67460333+Aryan-Dang@users.noreply.github.com>
2021-06-19 05:02:25 +0530

    Added code to get website and parse using BeautifulSoup
    Function added which :
    1) gets website's HTML source code
    2) parses using lxml parser and BeautifulSoup
    3) returns list of all sections matching the requirement

--- scrape.py
+++ scrape.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL= ""
+def get_soup(searchVal): 
+    URL= f"https://www.dictionary.com/browse/{searchVal}"
+    source = requests.get(URL).text
+    soup = BeautifulSoup(source,'lxml')
 
-source = requests.get(URL).text
-
-site = BeautifulSoup(source,'lxml')
-
-headers = site.find_all('header',class_='entry-header')
-
-for header in headers:
-    print(header.text)
+    section = 'css-pnw38j e1hk9ate4'
+    
+    #returns list of all <section> tags that have class attribute set to css-pnw38j e1hk9ate4
+    return soup.find_all('section',class_=section)
+    #Add stuff to do when no results come aka this won't exist
